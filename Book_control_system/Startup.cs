e02d81d66b6771efaf40b69fd404e5702e64d3bc
Kirill Apanasiuk <milidanex@gmail.com>
2020-10-06 14:01:36 +0300

    feat:dependencies

--- Book_control_system/Startup.cs
+++ Book_control_system/Startup.cs
@@ -26,8 +26,8 @@ namespace Book_control_system
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<BookControlSystemContext>(options => 
-            options.UseSqlServer(Configuration.GetConnectionString("BookControlSystem")));
+            services.AddDbContext<BookControlSystemContext>(options =>
+         options.UseSqlServer(Configuration.GetConnectionString("BookControlSystem")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
